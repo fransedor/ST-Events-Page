8e7a5db1a37ff46102857447caef62349e29875a
fransedor <fransedoravelino@gmail.com>
2021-08-12 19:00:11 +0700

    desktop navbar done

---
+++ events.js
@@ -0,0 +1,14 @@
+const events =  [
+        {
+            "name": "Starting Line",
+            "link": "https://www.seributujuan.id/startingline",
+            "poster": "https://uploads-ssl.webflow.com/5e9bb8df9aae7e8192b64336/5fe2e163000ba2ac41bfece1_Sl-Logo-Regular-heroExtended.svg",
+            "id": 1
+        },
+        {
+            "name": "Reconnect",
+            "link": "https://www.seributujuan.id/reconnect",
+            "poster": "https://uploads-ssl.webflow.com/5e9bb8df9aae7e8192b64336/60c457ac468769cee431351d_RC-title-hero-new-2.svg",
+            "id": 2
+        }
+    ]
---
+++ index.js
@@ -0,0 +1,17 @@
+const eventList = document.querySelector(".events-list");
+const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+
+const fontSize = vw/85.375;
+
+document.addEventListener("DOMContentLoaded", () => {
+    eventList.innerHTML = "";
+    events.forEach(event => {
+        eventPreview = document.createElement('div');
+        eventPreview.classList.add('events-preview');
+        eventPreview.innerHTML = `
+        <img src=${event.poster}></img>
+        <p>Learn More</p>`;
+        eventList.appendChild(eventPreview);
+    })
+    document.body.style.fontSize = `${fontSize}px`;
+})
